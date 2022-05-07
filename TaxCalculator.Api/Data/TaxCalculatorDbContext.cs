d7cf9196bdfd9610d486552e040c68617c97fbcd
Willem Steyn <leftknee4@gmail.com>
2022-05-07 07:05:17 +0200

    Started to add DB Entities and DB Context

---
+++ TaxCalculator.Api/Data/TaxCalculatorDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaxCalculator.Api.Entities;
+
+namespace TaxCalculator.Api.Data
+{
+    public class TaxCalculatorDbContext: DbContext
+    {
+        public TaxCalculatorDbContext(DbContextOptions<TaxCalculatorDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<TaxCalculatorLog> TaxCalculatorLogs { get; set; }
+    }
+}
---
+++ TaxCalculator.Api/Entities/TaxCalculatorLog.cs
@@ -0,0 +1,11 @@
+﻿namespace TaxCalculator.Api.Entities
+{
+    public class TaxCalculatorLog
+    {
+        public int Id { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PostalCode { get; set; }
+        public Double AnnualIncome { get; set; }
+        public Double TaxedValue { get; set; }
+    }
+}
