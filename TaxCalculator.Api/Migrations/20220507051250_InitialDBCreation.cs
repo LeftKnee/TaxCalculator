b2cd56593278c8534b20a4316deb3b90b9e73925
Willem Steyn <leftknee4@gmail.com>
2022-05-07 07:15:48 +0200

    Added initial db create migration

---
+++ TaxCalculator.Api/Migrations/20220507051250_InitialDBCreation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaxCalculator.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDBCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaxCalculatorLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnnualIncome = table.Column<double>(type: "float", nullable: false),
+                    TaxedValue = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaxCalculatorLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaxCalculatorLogs");
+        }
+    }
+}
--- TaxCalculator.Api/Program.cs
+++ TaxCalculator.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TaxCalculator.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<TaxCalculatorDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TaxCalculatorConnection"))
+);
 
 var app = builder.Build();
 
